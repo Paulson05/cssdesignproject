39dec00dcc00cb4e5e410649e5307d691ce04188
okofu <okofuonyebuchi@gmail.com>
2020-08-25 21:33:29 -0700

    tosin website

--- cssdesignaward.js
+++
@@ -1,6 +0,0 @@
-var hambuger = document.getElementById("hambuger-toggle");
-var navmenu =  document.getElementById("nav-menu");
-hambuger.addEventListener("click", function () {
-    hambuger.classList.toggle("change");
-    navmenu.classList.toggle("nav-toggle");
-})
